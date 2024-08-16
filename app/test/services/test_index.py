f1e51ac8b15ba545d6c7614ff7b78a21f9ed8713
Franco <31745377+Bade99@users.noreply.github.com>
2024-08-16 16:29:48 -0300

    refactor/FBA - PIZ8: Refactor Services (#6)
    * refactor/FBA - PIZ8: Added base Service functions
    
    * refactor/FBA - PIZ8: Implemented the common service functionality on all services

--- app/test/services/test_index.py
+++ app/test/services/test_index.py
@@ -3,4 +3,7 @@ import pytest
 
 def test_get_index_service(client, index_uri):
     response = client.get(index_uri)
-    pytest.assume(not response.json["error"])
+    result = response.json
+    pytest.assume(not result["error"])
+    pytest.assume("version" in result)
+    pytest.assume("status" in result)
